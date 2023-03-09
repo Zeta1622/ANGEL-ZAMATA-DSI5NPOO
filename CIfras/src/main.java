f65e1b304d496b9b1de9fb6c4fe7536a769daa6c
LAB <angelzamata17@gmail.com>
2023-03-09 18:27:17 -0500

    gaaaa

---
+++ CIfras/src/main.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class main{
+    public static void unidades(){
+        int unidad=0;
+        switch (unidad){
+
+        }
+    }
+    public static void cifras(int num){
+       num=0;
+       int unidad=num%10;
+       unidades();
+       int decena=(num%10)/10;
+
+    }
+
+
+}
