63df316e7419d1b69990aefa26c8b08f6e7f0614
Zeta1622 <angelzamata17@gmail.com>
2023-03-15 22:04:49 -0500

    Casi listo

--- CALENSARIO 2.0/src/Main.java
+++ CALENSARIO 2.0/src/Main.java
@@ -1,17 +1,24 @@
 
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("INGRESE UN AÑO: ");
         int nAño=new Scanner(System.in).nextInt();
-        for (int i=1; i<=12; i++){
-            System.out.println();
-            System.out.println("D L M M J V S");
+        for (int i=0; i<=11; i++){
+            System.out.println("\n");
+            String numMes[]={"ENERO","FEBRERO","MARZO","ABRIL","MAYO","JUNIO","JULIO","AGOSTO","SEPTIEMBRE","OCTUBRE","NOVIEMBRE","DICIEMBRE"};
+            System.out.println(numMes[i]);
+            System.out.println(" D  L  M  M  J  V  S");
             int dias=dMes (nAño, i);
             int contador=0;
-
-            for (int j=1; j<=dias; j++);{
-                System.out.println(j+"");
+            for (int j=1; j<=dias; j++){
+                if (j<10){
+                    System.out.print("0"+j+" ");
+                }else {
+                    System.out.print(j+" ");
+                }
                 contador++;
                 if (contador==7){
                     System.out.println();
@@ -20,11 +27,11 @@ public class Main {
             }
         }
     }
-    public static int dMes(int año, int mes){
+    public static int dMes(int nAño, int mes){
         if (mes==1 || mes==3 || mes==5 || mes==7 || mes==1 || mes==8 || mes==10 || mes==12 ){
             return 31;
         } else if (mes==2) {
-            if (esBisiesto(año)){
+            if (esBisiesto(nAño)){
                 return 29;
             }else{
                 return 28;
