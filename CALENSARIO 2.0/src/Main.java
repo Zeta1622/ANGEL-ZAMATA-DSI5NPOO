f459097921da4dff2fa379d96dfb97d65ed5a8d7
Zeta1622 <angelzamata17@gmail.com>
2023-03-16 14:49:53 -0500

    Terminado

--- CALENSARIO 2.0/src/Main.java
+++ CALENSARIO 2.0/src/Main.java
@@ -2,31 +2,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("INGRESE UN AÑO: ");
-        int nAño=new Scanner(System.in).nextInt();
-        for (int i=0; i<=11; i++){
-            System.out.println("\n");
-            String numMes[]={"ENERO","FEBRERO","MARZO","ABRIL","MAYO","JUNIO","JULIO","AGOSTO","SEPTIEMBRE","OCTUBRE","NOVIEMBRE","DICIEMBRE"};
-            System.out.println(numMes[i]);
-            System.out.println(" D  L  M  M  J  V  S");
-            int dias=dMes (nAño, i);
-            int contador=0;
-            for (int j=1; j<=dias; j++){
-                if (j<10){
-                    System.out.print("0"+j+" ");
-                }else {
-                    System.out.print(j+" ");
-                }
-                contador++;
-                if (contador==7){
-                    System.out.println();
-                    contador=0;
-                }
-            }
-        }
-    }
+  //DIAS DEL MES
     public static int dMes(int nAño, int mes){
         if (mes==1 || mes==3 || mes==5 || mes==7 || mes==1 || mes==8 || mes==10 || mes==12 ){
             return 31;
@@ -40,6 +16,7 @@ public class Main {
             return 30;
         }
     }
+    //AÑO BISISESTO O NO
     public static boolean esBisiesto(int año){
         if (año%4==0){
             if (año%100==0){
@@ -53,6 +30,43 @@ public class Main {
             }
         }else {
             return false;
+        }
+    }
+    public static int zeller(int nAño, int mes){
+        int a=(14-mes)/12;
+        int y= nAño - a;
+        int m= mes + 12*a-2;
+        int dia=1,d;
+        d=(dia+y+y/4-y/100+y/400+(31+m)/12)%7;
+        return (d);
+    }
+    public static void main(String[] args) {
+        System.out.println("INGRESE UN AÑO: ");
+        int nAño=new Scanner(System.in).nextInt();
+        for (int i=1; i<=12; i++){
+            System.out.println("\n");
+            String numMes[]={"ENERO","FEBRERO","MARZO","ABRIL","MAYO","JUNIO","JULIO","AGOSTO","SEPTIEMBRE","OCTUBRE","NOVIEMBRE","DICIEMBRE"};
+            System.out.println(numMes[i-1]);
+            System.out.println(" D  L  M  M  J  V  S");
+            int dias=dMes (nAño, i);
+            int ecZeller=zeller(nAño,i);
+            int contador=0;
+            for (int k=0;k<ecZeller;k++){
+                System.out.print(" . ");
+                contador++;
+            }
+            for (int j=1; j<=dias; j++){
+                if (j<10){
+                    System.out.print("0"+j+" ");
+                }else {
+                    System.out.print(j+" ");
+                }
+                contador++;
+                if (contador==7){
+                    System.out.println();
+                    contador=0;
+                }
+            }
         }
     }
 }
